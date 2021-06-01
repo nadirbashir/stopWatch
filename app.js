b60662fb92673acad3d262fac5348521b7ef12ea
nadirbashir <1.nadirbashir@gmail.com>
2021-06-02 03:26:02 +0500

    UI complete

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-var count = 0;
-var interval;
+// var count = 0;
+// var interval;
 
-function timer(){
-    count++;
-    console.log(count);
+// function timer(){
+//     count++;
+//     console.log(count);
 
-}
-
-// //set interval ek javascript ka default function hai jo apny aap kuch time badh repeat hota hai
-interval = setInterval(timer,1000);
-
-// //set interval ko setTimeout method se roksakty hain
-
-// // setTimeOut ye ek bar chalta hai or dhusry argument mein jo time dhengy utny time badh ye chalega
-// function timeOut(){
-//     console.log("running...");
 // }
 
-// // setTimeout(timeOut, 3000);
+// // //set interval ek javascript ka default function hai jo apny aap kuch time badh repeat hota hai
+// interval = setInterval(timer,1000);
 
-setTimeout(function(){
-   // //clearInterval by default method hai jo timer ko rokta 
-    clearInterval(interval);
-},5000)
+// // //set interval ko setTimeout method se roksakty hain
+
+// // // setTimeOut ye ek bar chalta hai or dhusry argument mein jo time dhengy utny time badh ye chalega
+// // function timeOut(){
+// //     console.log("running...");
+// // }
+
+// // // setTimeout(timeOut, 3000);
+
+// setTimeout(function(){
+//    // //clearInterval by default method hai jo timer ko rokta 
+//     clearInterval(interval);
+// },5000)
+
