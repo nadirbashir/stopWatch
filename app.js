f5f477972b8b8cbc62c0ea648d0c9c015b21f85d
nadirbashir <1.nadirbashir@gmail.com>
2021-06-04 21:16:45 +0500

    timer feature bug fix

--- app.js
+++ app.js
@@ -34,12 +34,47 @@ var msecHeading = document.getElementById("msec");
 var progress = document.getElementById("progress");
 var rightProgress = document.getElementById("rightProgress");
 var heading = document.getElementById("heading");
+var timerButtons = document.getElementById("timer_btn");
+var stopWatchButtons = document.getElementById("stopWatch_btn");
+var totalSec = 0;
 var interval;
 
-function clearScr() {
+function stopWatch_btn() {
+  clearInterval(interval);
+  heading.innerHTML = "Stop Watch";
+  document.getElementById("startStopWatch").removeAttribute("disabled");
+  min = 0;
+  sec = 0;
+  msec = 0;
+  minHeading.value = min;
+  secHeading.value = sec;
+  msecHeading.value = msec;
+  timerButtons.style.display = "none";
+  stopWatchButtons.style.display = "block";
   minHeading.style.border = "none";
   secHeading.style.border = "none";
   msecHeading.style.border = "none";
+  progress.style.width = "0%";
+  progress.style.height = "0%";
+  rightProgress.style.width = "0%";
+  rightProgress.style.height = "0%";
+
+}
+
+function setTimer() {
+  minHeading.value = 0;
+  secHeading.value = 0;
+  msecHeading.value = 0;
+  stopWatchButtons.style.display = "none";
+  timerButtons.style.display = "block";
+  heading.innerHTML = "Timer";
+  minHeading.focus();
+  secHeading.style.border = "1px solid #c3b20f";
+  msecHeading.style.border = "1px solid #c3b20f";
+  progress.style.width = "0%";
+  progress.style.height = "0%";
+  rightProgress.style.width = "0%";
+  rightProgress.style.height = "0%";
 }
 
 function stopWatch() {
@@ -67,61 +102,70 @@ function timer() {
     min--;
     minHeading.value = min;
     sec = 60;
-  } else if (min < 0) {
-    clearInterval(interval);
+  } else if (min < 0){
     reset();
+    clearInterval(interval);
   }
 }
 
-function start() {
-  if (
-    minHeading.value == 0 &&
-    secHeading.value == 0 &&
-    msecHeading.value == 0
-  ) {
+function startStopWatch() {
     interval = setInterval(stopWatch, 10);
-    document.getElementById("btnStart").setAttribute("disabled", "");
-  } else {
+    document.getElementById("startStopWatch").setAttribute("disabled", ""); 
+}
+
+function startTimer(){
+  min = minHeading.value;
+  sec = secHeading.value;
+  msec = msecHeading.value;
+  interval = setInterval(timer, 10);
+  document.getElementById("startTimer").setAttribute("disabled", "");
+  if(rightProgress.style.animationPlayState === 'paused' || progress.style.animationPlayState === 'paused' && (totalSec == 0)){
+    rightProgress.style.animationPlayState = 'running';
+    progress.style.animationPlayState = 'running'
+  }else{
+    console.log(totalSec + "total sec")
     minHeading.style.border = "none";
-    secHeading.style.border = "none";
-    msecHeading.style.border = "none";
-    min = minHeading.value;
-    sec = secHeading.value;
-    msec = msecHeading.value;
-    var totalSec = (min * 60 + +sec) / 2;
-    interval = setInterval(timer, 10);
-    document.getElementById("btnStart").setAttribute("disabled", "");
-    progress.style.animationDuration = totalSec + 3 + "s";
-    // rightProgress.style.animationDelay = '6s';
-    rightProgress.style.animationDuration = totalSec + "s";
-    rightProgress.style.animationDelay = totalSec + 3 + "s";
-    rightProgress.style.width = "100%";
-    rightProgress.style.height = "100%";
-    progress.style.width = "100%";
-    progress.style.height = "100%";
-  }
+  secHeading.style.border = "none";
+  msecHeading.style.border = "none";
+  totalSec = ((min * 60 + (+sec)) / 2);
+  rightProgress.style.animationDuration = totalSec + "s";
+  rightProgress.style.animationDelay = 3 + "s";
+  // rightProgress.style.animationIterationCount = 1;
+  rightProgress.style.width = "100%";
+  rightProgress.style.height = "100%";
+  progress.style.animationDuration = +totalSec + 3 + "s";
+  rightProgress.style.animationDelay =+ totalSec + 3 + "s";
+  // progress.style.animationIterationCount = 1;
+  progress.style.width = "100%";
+  progress.style.height = "100%";
+  // console.log(progress.style.animationDuration);
+  // rightProgress.style.animationDelay = '6s';
+}
 }
 
 function pause() {
   clearInterval(interval);
-  document.getElementById("btnStart").removeAttribute("disabled");
+  document.getElementById("startStopWatch").removeAttribute("disabled");
+  document.getElementById("startTimer").removeAttribute("disabled");
+  rightProgress.style.animationPlayState = 'paused';
+  progress.style.animationPlayState = 'paused';
 }
 function reset() {
   pause();
   min = 0;
   sec = 0;
   msec = 0;
+  totalSec = 0;
   minHeading.value = min;
   secHeading.value = sec;
   msecHeading.value = msec;
+  progress.style.width = "0%";
+  progress.style.height = "0%";
+  rightProgress.style.width = "0%";
+  rightProgress.style.height = "0%";
 }
 
-function setTimer() {
-  minHeading.focus();
-  secHeading.style.border = "1px solid #c3b20f";
-  msecHeading.style.border = "1px solid #c3b20f";
-  heading.innerHTML = "Timer";
-}
+
 
 function borderYellow(x) {
   x.style.border = "1px solid #c3b20f";
